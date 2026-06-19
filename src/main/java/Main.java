55c835698b58619fba3b86a52ad1c8c5a5cfe6be
Swapnil Patil <nil.patil2809@gmail.com>
2026-06-19 16:14:16 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
         Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        System.out.println(s+": command not found");
+        while(true)
+        {
+            System.out.print("$ ");
+            String s = sc.nextLine();
+            System.out.println(s+": command not found");
+        }
     }
 }
