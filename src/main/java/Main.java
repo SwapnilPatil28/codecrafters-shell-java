ed6398aa7158872284ba912e4d2b08ad6accc9ec
Swapnil Patil <nil.patil2809@gmail.com>
2026-06-19 16:35:20 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,9 +8,14 @@ public class Main {
             System.out.print("$ ");
             String s = sc.nextLine();
             if(s.equals("exit"))   break;
-            else if(s.startsWith("echo"))
+            else if(s.startsWith("echo "))
             {
                 System.out.println((s.length()>5)?s.substring(5):"");
+            }
+            else if(s.startsWith("type "))
+            {
+                if(s.substring(5).equals("exit")||s.substring(5).equals("echo")||s.substring(5).equals("type"))     System.out.println(s.substring(5)+" is a shell builtin");
+                else System.out.println(s.substring(5)+": not found");
             }
             else System.out.println(s+": command not found");
         }
